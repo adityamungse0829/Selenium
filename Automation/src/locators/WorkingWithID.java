4560f84bb70aff607b70dbe4f0aaab969e5f3f67
adityamungse <adityamungse@localhost>
2024-09-01 16:34:39 +0530

    Done andf dusted

--- Automation/src/locators/WorkingWithID.java
+++ Automation/src/locators/WorkingWithID.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WorkingWithID {
 	public static void main(String[] args) {
 		
+		
+		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
